7b49cbee5249750b7d01d2dc06db8344d6dc6b48
Ryan Teixeira <ryantxr@gmail.com>
2025-08-07 20:00:13 -0400

    Update SheetClient.php

--- src/SheetClient.php
+++ src/SheetClient.php
@@ -131,19 +131,5 @@ class SheetClient
     }
 }
 
-class TooManyRequestsException extends \RuntimeException
-{
-    private ?int $retryAfter;
 
-    public function __construct(?int $retryAfter = null, ?\Throwable $previous = null)
-    {
-        $this->retryAfter = $retryAfter;
-        parent::__construct('Too Many Requests', 429, $previous);
-    }
-
-    public function getRetryAfter(): ?int
-    {
-        return $this->retryAfter;
-    }
-}
 
